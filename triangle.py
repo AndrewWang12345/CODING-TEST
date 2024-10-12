733041870d8394499cffa1b49f931e7bc9bc3092
Andrew Wang <144137738+AndrewWang12345@users.noreply.github.com>
2024-10-11 22:23:02 -0400

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,7 @@
+#the question doesn't say what character or what shape the triangle is to be made with, so I made a right triangle out of *
+def triangle_3x4(): print('*\n**\n****') # it is impossible to make a perfect 3x4 triangle(3 *s high and 4 *s width)
+  #so I chose to round each row to the nearest whole number of *s for row, meaning the hypotenuse will not be perfectly straight, as shown below.
+#*
+#**
+#****
+
